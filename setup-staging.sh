231025230f627d62c50ee1fcef1f535b22d8fa02
therealpaulgg <paul@paul.systems>
2019-08-01 23:36:51 -0700

    build: prod script

---
+++ setup-staging.sh
@@ -0,0 +1,10 @@
+echo "Please install packages manually for staging/prod."
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+envsubst < prod-env > .env
+if [[ ! -d migrations ]]; then
+    flask db init
+    flask db migrate
+    flask db upgrade
+fi

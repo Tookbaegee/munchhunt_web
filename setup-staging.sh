c25a4146bd086f65ef7adbff8bfcf9f109d01be1
therealpaulgg <paul@paul.systems>
2019-08-01 23:40:12 -0700

    oops

--- setup-staging.sh
+++ setup-staging.sh
@@ -2,7 +2,7 @@ echo "Please install packages manually for staging/prod."
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-envsubst < prod-env > .env
+envsubst < staging-env > .env
 if [[ ! -d migrations ]]; then
     flask db init
     flask db migrate
